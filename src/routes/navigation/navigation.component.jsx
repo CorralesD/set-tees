e2dd85a668a6d1181a5a9fe6b9aedfb26cbbb773
Diego Corrales <dcorrales@brooksbell.com>
2023-08-21 13:49:23 -0700

    added routes and nav component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,38 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    const navLinks = [
+        {
+            id: 1,
+            title: 'Shop',
+            route: 'shop'
+        }
+    ];
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <Logo className='logo' />
+                </Link>
+                <div className='nav-links-container'>
+                    {navLinks.map((navLink) => (
+                        <Link className='nav-link' key={navLink.id} to={navLink.route}> {navLink.title} </Link>
+                        )
+                    )}
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+}
+
+export default Navigation;
+
+// {categories.map((category) => (
+//     <CategoryItem key={category.id} category={category} />
+//   ))}
